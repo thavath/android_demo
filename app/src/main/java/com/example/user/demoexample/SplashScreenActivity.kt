a858dc8208fc4237283a9580b500f6c50cb8cdf5
thavath <thavathrath@gmail.com>
2018-09-17 11:22:08 +0700

    add splash screen

---
+++ app/src/main/java/com/example/user/demoexample/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.user.demoexample
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed( Runnable{
+            kotlin.run {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 3000)
+    }
+}
